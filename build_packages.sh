ca46b97f2e25b3e91998575a452618ee832bc15e
SteMuc <stefanoangeli12@gmail.com>
2024-04-28 17:06:53 +0200

    Added minor changes.

--- build_packages.sh
+++ build_packages.sh
@@ -2,9 +2,7 @@ source /ros_entrypoint.sh
 sudo rm -rf build devel log
 sudo rosdep install --from-paths src --ignore-src --rosdistro humble -y --skip-keys libfranka
 colcon build --symlink-install
-source install/setup.bash
 
-if ! grep -q "source /workspaces/install/setup.bash" ~/.bashrc; then
-    # If not present, add the line to ~/.bashrc
-    echo "source /workspaces/install/setup.bash" >> ~/.bashrc
-fi
+source install/setup.bash
+#source workspaces
+echo 'Source workspace'
